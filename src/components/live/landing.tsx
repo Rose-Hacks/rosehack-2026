85c35a71b02e6476e9692b755d70dc4d23463a08
Charleen Chen <charleenc485@gmail.com>
2025-06-19 22:43:35 -0700

    oops this should actually push it now

--- src/components/live/landing.tsx
+++ src/components/live/landing.tsx
@@ -1,15 +1,82 @@
-import Countdown from "../ui/countdown";
+import Image from "next/image";
+import Logo from "@/public/engineering/logo.svg";
+import Content from "./landing/content";
+
+import tree1 from "@/public/engineering/landing/tree1.svg";
+import tree2 from "@/public/engineering/landing/tree2.svg";
+import treeBackground1 from "@/public/engineering/landing/tree_background1.svg";
+import treeBackground2 from "@/public/engineering/landing/tree_background2.svg";
+import light from "@/public/engineering/landing/light.svg";
+import bush from "@/public/engineering/landing/bush.svg";
+import path from "@/public/engineering/landing/path.svg";
+
 const Landing = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <Countdown
-        classNames={{
-          unit: "text-black",
-          background: "bg-hackathon-blue-100",
-          digit: "text-white",
-        }}
+    <div className="relative flex h-screen w-full flex-col items-center justify-center bg-gradient-to-b from-[#F77652] via-[#F55654] to-[#98436B]">
+      <a
+        id="mlh-trust-badge"
+        href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed right-[90px] top-0 z-[10000] block w-[10%] min-w-[60px] max-w-[100px]"
+      >
+        <Image
+          src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg"
+          alt="Major League Hacking 2026 Hackathon Season"
+          className="w-full"
+          width={100}
+          height={100}
+        />
+      </a>
+
+      <Image src={tree1} alt="tree" className="absolute left-0 z-50" />
+      <Image src={bush} alt="bush" className="absolute bottom-0 left-10 z-30" />
+      <Image src={path} alt="path" className="absolute bottom-0 left-10 z-20" />
+      <Image
+        src={treeBackground1}
+        alt="tree"
+        className="absolute -top-4 left-2 z-0 scale-125"
       />
-      Rose Hack 2026 OMG!!
+      <Image
+        src={light}
+        alt="light"
+        className="absolute -left-[100px] top-52 z-0 scale-[0.7]"
+      />
+      <Image
+        src={light}
+        alt="light"
+        className="absolute -top-40 left-10 z-50 scale-[0.7]"
+      />
+      <div className="absolute top-32 z-50 flex w-fit flex-col items-center justify-center gap-4 md:top-36 md:w-full">
+        <Image src={Logo} alt="Rose Hack Logo" className="w-1/12" />
+        <Content />
+      </div>
+      <Image
+        src={light}
+        alt="light"
+        className="absolute -top-20 right-2 z-50 scale-[0.4]"
+      />
+      <Image
+        src={light}
+        alt="light"
+        className="absolute -right-[170px] top-52 z-50 scale-[0.7]"
+      />
+      <Image
+        src={light}
+        alt="light"
+        className="absolute -bottom-5 right-20 z-50 scale-[0.7]"
+      />
+      <Image src={tree2} alt="tree" className="absolute right-0 z-30" />
+      <Image
+        src={treeBackground2}
+        alt="tree"
+        className="absolute right-0 z-10"
+      />
+      <Image
+        src={bush}
+        alt="bush"
+        className="absolute -right-10 bottom-0 z-20 scale-x-[-1]"
+      />
     </div>
   );
 };
---
+++ src/components/live/landing/content.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import Button from "@/components/ui/button";
+import { InstagramIcon, LinkedinIcon, MailIcon } from "lucide-react";
+import Countdown from "@/components/ui/countdown";
+
+const Content = () => {
+  return (
+    <div className="items-centerjustify-center flex w-11/12 flex-col gap-2 md:w-3/5 xl:w-2/5">
+      <div className="flex w-full flex-col items-center justify-center gap-4 rounded-3xl p-3">
+        <div className="whitespace-nowrap rounded-3xl text-center font-rosehack-main text-lg text-white md:whitespace-normal md:text-6xl">
+          ROSE HACK 2026
+          <div className="m-3 flex items-center justify-center whitespace-nowrap rounded-3xl px-7 text-center text-lg md:whitespace-normal md:text-3xl">
+            COMING SOON
+          </div>
+          <Countdown
+            classNames={{
+              unit: "text-white",
+              background: "bg-white/20",
+              digit: "text-white",
+            }}
+          />
+        </div>
+
+        <div className="grid grid-cols-1 items-center justify-center gap-3 p-5 font-rosehack-alt duration-200 hover:scale-105">
+          <Button text="Interest Form" link="/form/interest" />
+        </div>
+        <div className="flex flex-row gap-3 text-white">
+          <Link
+            href="https://www.instagram.com/rosehackucr/"
+            className="duration-200 hover:scale-105"
+          >
+            <InstagramIcon className="size-9 md:size-10" />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/company/33212536"
+            className="duration-200 hover:scale-105"
+          >
+            <LinkedinIcon className="size-9 md:size-10" />
+          </Link>
+          <Link
+            href="mailto:rosehackucr@gmail.com"
+            className="duration-200 hover:scale-105"
+          >
+            <MailIcon className="size-9 md:size-10" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
