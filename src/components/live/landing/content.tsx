ee4140d988f8204abcde49ab5d85319b51e9174c
Kevin Loritsch <kloritsch58@gmail.com>
2025-06-19 23:33:13 -0700

    tweaks and mobile

--- src/components/live/landing/content.tsx
+++ src/components/live/landing/content.tsx
@@ -23,7 +23,7 @@ const Content = () => {
 
         <div className="grid grid-cols-1 items-center justify-center gap-3 p-5 font-rosehack-alt duration-200 hover:scale-105">
           <Button className="border-rosehack-white/30 hover:border-rosehack-white/40 flex items-center justify-center rounded-xl border-4 bg-rosehack-yellow px-10 text-xl text-gray-800 duration-150 hover:cursor-pointer hover:bg-rosehack-yellow/80">
-            <Link href="/form/interest">Interest Form</Link>
+            <Link href="/apply/interest">Interest Form</Link>
           </Button>
         </div>
         <div className="flex flex-row gap-3 text-white">
