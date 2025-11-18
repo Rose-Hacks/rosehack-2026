623a9cc6d7d9cd74cfe19554c1519f3b593886cf
ACM UCR <fake@domain.com>
2025-11-18 00:40:30 +0000

    convert images to webp

--- src/data/team.ts
+++ src/data/team.ts
@@ -1,15 +1,15 @@
 import { StaticImageData } from "next/image";
 
 import Alessandro from "@/public/engineering/landing/headshots/Alessandro.webp";
-import Arielle from "@/public/engineering/landing/headshots/Arielle.jpg";
+import Arielle from "@/public/engineering/landing/headshots/Arielle.webp";
 import Charleen from "@/public/engineering/landing/headshots/Charleen.JPG";
 import Fatima from "@/public/engineering/landing/headshots/Fatima.webp";
 import Jasmine from "@/public/engineering/landing/headshots/Jasmine.JPG";
-import Jia from "@/public/engineering/landing/headshots/Jia.jpeg";
+import Jia from "@/public/engineering/landing/headshots/Jia.webp";
 import Kaylie from "@/public/engineering/landing/headshots/Kaylie.JPG";
 import Kevin from "@/public/engineering/landing/headshots/Kevin.JPG";
 import Lydia from "@/public/engineering/landing/headshots/Lydia.JPG";
-import Michelle from "@/public/engineering/landing/headshots/Michelle.jpg";
+import Michelle from "@/public/engineering/landing/headshots/Michelle.webp";
 import Quin from "@/public/engineering/landing/headshots/Quin.JPG";
 import Selina from "@/public/engineering/landing/headshots/Selina.JPG";
 import Shane from "@/public/engineering/landing/headshots/Shane.JPG";
