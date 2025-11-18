977900951a653c31986c477ccddd9640c9d50077
Kevin Loritsch <kloritsch58@gmail.com>
2025-11-17 17:18:03 -0800

    fully compressed webp

--- src/components/live/team.tsx
+++ src/components/live/team.tsx
@@ -3,6 +3,8 @@ import Member from "./member";
 import Teams from "@/data/team";
 
 const Team = () => {
+  const directors = Teams.slice(0, 2);
+  const leads = Teams.slice(2);
   return (
     <div
       className="h-auto py-2 md:py-6"
@@ -14,7 +16,17 @@ const Team = () => {
         <Title>Meet The Team</Title>
       </div>
       <div className="mx-4 flex flex-col place-items-center md:mx-8 md:flex-row md:flex-wrap md:items-center md:justify-center">
-        {Teams.map(({ name, image, role }, index) => (
+        {directors.map(({ name, image, role }, index) => (
+          <div
+            key={index}
+            className="relative z-20 m-2 flex w-10/12 flex-col items-center justify-center lg:w-1/4"
+          >
+            <Member name={name} role={role} image={image} />
+          </div>
+        ))}
+      </div>
+      <div className="mx-4 flex flex-col place-items-center md:mx-8 md:flex-row md:flex-wrap md:items-center md:justify-center">
+        {leads.map(({ name, image, role }, index) => (
           <div
             key={index}
             className="relative z-20 m-2 flex w-10/12 flex-col items-center justify-center lg:w-1/4"
